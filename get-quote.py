6cbaa8453595dd57733a4002a44906402e7cef24
J B <sjcy-5@CYS5-KALI01.cyber.int>
2021-02-18 21:20:35 -0400

    Random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,18 @@
+import random
+
 def primary():
     print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    
+    last = len(quotes) -1
+    rnd = random.randint(0, last)
+    print("This is the first line of quote.txt: "+quotes[0])
+    print("This is the last line of quote.txt: "+quotes[13])
 
-  #print(quotes)
+    print("This is a randomly generated line: "+quotes[rnd])
 
 if __name__== "__main__":
   primary()
